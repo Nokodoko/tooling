85490dd6e8b2c972396f85d345554fc07d6499cd
Nokodoko <cMonty614@gmail.com>
2022-02-19 17:56:26 -0500

    added verbosity to terraform commands

--- terraform.zsh
+++ terraform.zsh
@@ -1,17 +1,17 @@
 export TF_VAR_avail_zone="us-east-1a"
 
-alias tf='terraform'
-alias tfi='terraform init'
-alias tfs='terraform state'
+alias tf='TF_LOG=DEBUG OCI_GO_SDK_DEBUG=v terraform'
+alias tfi='TF_LOG=DEBUG OCI_GO_SDK_DEBUG=v terraform init'
+alias tfs='TF_LOG=DEBUG OCI_GO_SDK_DEBUG=v terraform state'
 
 function tfp() {
-    terraform plan -var-file $1 
+    TF_LOG=DEBUG OCI_GO_SDK_DEBUG=v terraform plan -lock=false -var-file $1 
 } 
 
 function tfa(){
-    terraform apply -var-file $1 -auto-approve
+    TF_LOG=DEBUG OCI_GO_SDK_DEBUG=v terraform apply -lock=false -var-file $1 -auto-approve
 } 
 
 function tfd(){
-    terraform destroy -var-file $1 -auto-approve
+    TF_LOG=DEBUG OCI_GO_SDK_DEBUG=v terraform destroy -lock=false -var-file $1 -auto-approve
 } 
