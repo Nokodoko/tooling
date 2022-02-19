9e5ffd9e3fb488de0fdb4f284d4dfcce82de4e7e
Nokodoko <cMonty614@gmail.com>
2022-02-19 17:56:04 -0500

    organized directories file

--- directories.zsh
+++ directories.zsh
@@ -1,8 +1,4 @@
 #------DIRECTORIES------#
-alias zconf='c ~/.zsh/tooling/'
-alias plug='c ~/.local/share/nvim/site/pack/packer/start/'
-alias vconf='c ~/.config/nvim/lua/n0ko/'
-alias conf='c ~/.config'
 alias exe='c /usr/local/bin'
 alias down='c ~/Downloads'
 alias resume='c ~/Downloads/resumes'
@@ -10,30 +6,49 @@ alias prog='c ~/Programs'
 alias vid='c ~/Videos'
 alias pic='c ~/Pictures'
 alias udemy='c ~/Videos/Udemy'
-alias goclass='c ~/Videos/Udemy/Go/learn-how-to-code/'
 alias mit='c ~/MIT'
-alias gosrc='c ~/go/src'
-alias gol='c ~/go/src/'
-alias gu='c ~/goUltimate v main.go'
-alias proj='c ~/project_1/'
-alias gtrain='c ~/gotraining'
-alias pprof='go tool pprof'
-alias xre='v ~/.Xresources'
-alias change='xrdb ~/.Xresources'
-alias vimrc='v ~/.config/nvim/init.vim'
-alias sv='sudo nvim'
 alias music='c ~/music'
 alias ws='wiki-search'
 alias manual='c ~/manuals/'
-alias alg='c ~/algorithmswithgo.com/module01 &&'
-alias play='c ~/goPlay'
+#arch
 alias arch='c /usr/share/doc/arch-wiki/html/en/'
-alias swapped='rm ~/.local/share/nvim/swap//%home%n0ko%.config%nvim%init.vim.swp'
-alias swap='rm ~/.local/share/nvim/swap//%home%n0ko%.zsh.swp'
+
+#credentials
 alias aconf='v ~/.aws/credentials'
+
+#fonts
 alias fonts='c /usr/share/fonts/'
 alias lsp='c /home/n0ko/.local/share/nvim/site/pack/packer/start/nvim-lspconfig/lua/lspconfig/server_configurations'
+
+#Capacity
 alias repo='c ~/capacity/repos/'
 alias dev='c ~/capacity/repos/dev/'
 alias ops='c ~/capacity/repos/dev/ops/'
 alias serv='c ~/capacity/repos/dev/services'
+
+#nvim directories
+alias plug='c ~/.local/share/nvim/site/pack/packer/start/'
+alias plug='c ~/.local/share/nvim/site/pack/packer/start/'
+alias sv='sudo nvim'
+alias swapped='rm ~/.local/share/nvim/swap//%home%n0ko%.config%nvim%init.vim.swp'
+alias swap='rm ~/.local/share/nvim/swap//%home%n0ko%.zsh.swp'
+
+#golang
+alias alg='c ~/algorithmswithgo.com/module01 &&'
+alias play='c ~/goPlay'
+alias gtrain='c ~/gotraining'
+alias gosrc='c ~/go/src'
+alias goclass='c ~/Videos/Udemy/Go/learn-how-to-code/'
+alias pprof='go tool pprof'
+alias gu='c ~/goUltimate v main.go'
+
+#configs
+alias zconf='c ~/.zsh/tooling/'
+alias vconf='c ~/.config/nvim/lua/n0ko/'
+alias conf='c ~/.config'
+alias vimrc='v ~/.config/nvim/init.vim'
+
+#xorg
+alias proj='c ~/project_1/'
+alias xre='v ~/.Xresources'
+alias change='xrdb ~/.Xresources'
