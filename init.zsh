628ec113a92b92df94a47e9e48fa50a9f62fed02
chris <chris.montgomery@capacity.com>
2022-02-24 23:23:05 -0500

    updated init file

--- init.zsh
+++ init.zsh
@@ -1,8 +1,8 @@
 #Make an init and shutdown function
 function awsinit(){
-    sed -i 's-#aws.zsh-source ~/.zsh/tooling/aws.zsh-' .zshrc
+    sed -i 's-#aws.zsh-source ~/.zsh/tooling/aws.zsh-' ~/.zshrc
     source ~/.zshrc
 }
 function awsoff(){
-    sed -i 's-source ~/.zsh/tooling/aws.zsh-#aws.zsh-' .zshrc
+    sed -i 's-source ~/.zsh/tooling/aws.zsh-#aws.zsh-' ~/.zshrc
 }
