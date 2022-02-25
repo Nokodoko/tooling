e425df8e82fc20f29b50e3a7ea53cf3d0b49ff0f
chris <chris.montgomery@capacity.com>
2022-02-24 23:28:37 -0500

    added exports and starship

---
+++ exports.zsh
@@ -0,0 +1,16 @@
+export TERM="rxvt-256color"
+export COLORTERM='rxvt-unicode-256color'
+export EDITOR='nvim'
+export KUBE_EDITOR='nvim'
+export BROWSER='qutebrowser'
+export TERMINAL='kitty'
+export MANPAGER="nvim -c 'set ft=man' -"
+export LYNX_CFG='~/.config/lynx/lynx.cfg'
+export LESS_TERMCAP_mb=$'\e[1;32m'
+export LESS_TERMCAP_md=$'\e[1;32m'
+export LESS_TERMCAP_me=$'\e[0m'
+export LESS_TERMCAP_se=$'\e[0m'
+export LESS_TERMCAP_so=$'\e[01;33m'
+export LESS_TERMCAP_ue=$'\e[0m'
+export LESS_TERMCAP_us=$'\e[1;4;31m'
+
