908810140a456065319949321722b2229f226d13
chris <chris.montgomery@capacity.com>
2022-03-17 16:17:28 -0400

    updated with other kubectl shortcuts

--- k8.zsh
+++ k8.zsh
@@ -4,6 +4,8 @@ alias kstagef='kubectx staging-fuji'
 alias kserv='kubectx services'
 alias kprod='kubectx production'
 alias kgn='kubectl get nodes -o wide'
+alias kgs='kubectl get service -o wide'
+alias kds='kubectl describe service -o wide'
 alias kgp='kubectl get pods -o wide'
 alias kns='kubens'
 function kpf() {
