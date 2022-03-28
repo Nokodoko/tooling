bc464a35f5ccba9a7a787271af62d377fb32152e
chris <chris.montgomery@capacity.com>
2022-03-27 21:26:09 -0400

    adding helm shortcuts

--- helm.zsh
+++ helm.zsh
@@ -1,4 +1,7 @@
+alias helmh='cat ~/.zsh/tooling/helm.zsh'
 alias h='helm'
+alias hsh='helm search hub'
+alias hsr='helm search repo'
 alias hr='helm repo'
 function hadd() {
     helm repo add $1 https://charts.$1.com/$1
