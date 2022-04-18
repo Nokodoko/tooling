7c408d427fd97cd40296d9812980160039417faa
chris <chris.montgomery@capacity.com>
2022-04-18 19:16:09 -0400

    updated export

--- exports.zsh
+++ exports.zsh
@@ -13,4 +13,4 @@ export LESS_TERMCAP_se=$'\e[0m'
 export LESS_TERMCAP_so=$'\e[01;33m'
 export LESS_TERMCAP_ue=$'\e[0m'
 export LESS_TERMCAP_us=$'\e[1;4;31m'
-export GIT_ACROSS_FILESYSTEM=1
+export GIT_DISCOVERY_ACROSS_FILESYSTEM=1
