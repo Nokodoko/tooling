03ba6129cf1648b5dba3720b5aea598f76d132a0
chris <chris.montgomery@capacity.com>
2022-04-29 02:09:56 -0400

    updated dunstrc

--- dunst.zsh
+++ dunst.zsh
@@ -1,6 +1,8 @@
 alias d='dunstctl'
 alias dhp='dunstctl history-pop'
 alias dfy='dunstify'
+alias dpause='dunstctl set-paused true'
+alias dresume='dunstctl set-paused false'
 alias dprint='dunst --print'
 function dtest() {
     pidof dunst && killall dunst
